9456d14c5d490f0f82ed850acc5cceec053f5ccc
vikas0713 <vikas0713@gmail.com>
2021-04-16 11:24:09 +0530

    Added random library to generate random number for guess

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
+    let secret_number = rand::thread_rng().gen_range(1,101);
     println!("Input any number: ");
     let mut guess = String::new();  // Creating mutable variable
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!!");
+    println!("Secret number was: {}", secret_number);
     println!("You guessed: {}", guess);
 }
